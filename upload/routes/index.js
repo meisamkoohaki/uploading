e672795f65de1574faea23108e07d033a5b98e9c
Meisam <200412272@student.georgianc.on.ca>
2020-06-11 08:29:57 -0400

    Complete

--- upload/routes/index.js
+++ upload/routes/index.js
@@ -1,10 +1,42 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
+var message = "";
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+
+    var result = 0;
+    var x = parseFloat(req.query.x);
+    var y = parseFloat(req.query.y);
+    var method = req.query.method;
+
+    if (method == "add") {
+        result = x + y;
+        console.log(x + " + " + y + " = " + result);
+        message = x + " + " + y + " = " + result;
+    } else if (method == "multiple") {
+        result = x * y;
+        console.log(x + " * " + y + " = " + result);
+        message = x + " * " + y + " = " + result;
+    } else if (method == "subtract") {
+        result = x - y;
+        console.log(x + " - " + y + " = " + result);
+        message = x + " - " + y + " = " + result;
+    } else if (method == "divide") {
+        result = x / y;
+        console.log(x + " / " + y + " = " + result);
+        message = x + " / " + y + " = " + result;
+    } else if (method == "" || method) {
+        console.log("Error -------------<< input meyhod is wrong!>>");
+        message = "Error -------------<< input meyhod is wrong!>>";
+    }
+
+
+    res.render('index', {
+        title: 'Express',
+        message: message 
+    });
 });
 
 module.exports = router;
